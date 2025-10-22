392f4380039135c405ee3dcff3cb3b40a07274c6
asghar <asgharnasiri1990@gmail.com>
2025-10-22 18:59:44 +0330

    2nd

--- app1.js
+++ app1.js
@@ -1,2 +1,6 @@
-const add=require('./math');
-console.log(add(5,4))
+// const add=require('./math');
+// console.log(add(5,4))
+const os=require('os')
+var totalmem=os.networkInterfaces
+console.log(`totalmemory: ${totalmem}`)
+console.log("hello")
